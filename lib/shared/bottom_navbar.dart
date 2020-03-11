86f3212c6bf19ac8143055c9e35b6cd4c0b66ce2
Olafur <olafurao16@ru.is>
2020-03-11 22:10:55 +0000

    navbar look ready

--- lib/shared/bottom_navbar.dart
+++ lib/shared/bottom_navbar.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -13,54 +14,105 @@ class _BottomBarState extends State<BottomBar> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 50,
-      color: Colors.grey[800],
-      child: Row(
-        children: <Widget> [
-          Expanded(
-            child: InkWell(
-              child: Icon(
-                FontAwesomeIcons.solidBell,
-                color: selectedIndex == 0 ? highLightColor : Colors.white,
+    return Stack(
+      //mainAxisAlignment: MainAxisAlignment.end,
+      children: <Widget>[
+        Container(
+          height: 50,
+          color: Colors.grey[800],
+          child: Row(
+            children: <Widget> [
+              Expanded(
+                child: Container(
+                  padding: EdgeInsets.fromLTRB(0, 24, 0, 0),
+                  decoration: BoxDecoration(
+                    border: selectedIndex == 0 ? Border(
+                      top: BorderSide(
+                        width: 5,
+                        color: Colors.blue,
+                      ),
+                    ) : null,
+                  ),
+                  child: Container(
+                    transform: Matrix4.translationValues(0.0, -15.0, 0.0),
+                    child: InkWell(
+                      child: Icon(
+                        FontAwesomeIcons.solidBell,
+                        color: selectedIndex == 0 ? highLightColor : Colors.white,
+                      ),
+                      onTap: () {
+                        setState(() {
+                          selectedIndex = 0;
+                        });
+                      },
+                    ),
+                  ),
+                ),
               ),
-              onTap: () {
-                setState(() {
-                  selectedIndex = 0;
-                });
-              },
-            ),
-          ),
 
-          Expanded(
-            child: InkWell(
-              child: Icon(
-                FontAwesomeIcons.solidCommentAlt,
-                color: selectedIndex == 1 ? highLightColor : Colors.white,
+              Expanded(
+                child: Container(
+                  padding: EdgeInsets.fromLTRB(0, 24, 0, 0),
+                  decoration: BoxDecoration(
+                    border: selectedIndex == 1 ? Border(
+                      top: BorderSide(
+                        width: 5,
+                        color: Colors.blue,
+                      ),
+                    ) : null,
+                  ),
+                  child: Container(
+                    transform: Matrix4.translationValues(0.0, -15.0, 0.0),
+                    child: InkWell(
+                      child: Icon(
+                        FontAwesomeIcons.solidCommentAlt,
+                        color: selectedIndex == 1 ? highLightColor : Colors.white,
+                      ),
+                      onTap: () {
+                        setState(() {
+                          selectedIndex = 1;
+                        });
+                      },
+                    ),
+                  ),
+                ),
               ),
-              onTap: () {
-                setState(() {
-                  selectedIndex = 1;
-                });
-              },
-            ),
-          ),
 
-          Expanded(
-            child: InkWell(
-              child: Icon(
-                FontAwesomeIcons.userFriends,
-                color: selectedIndex == 2 ? highLightColor : Colors.white,
+              Expanded(
+                child: Container(
+                  padding: EdgeInsets.fromLTRB(0, 24, 0, 0),
+                  decoration: BoxDecoration(
+                    border: selectedIndex == 2 ? Border(
+                      top: BorderSide(
+                        width: 5,
+                        color: Colors.blue,
+                      ),
+                    ) : null,
+                  ),
+                  child: Container(
+                    transform: Matrix4.translationValues(0.0, -15.0, 0.0),
+                    child: InkWell(
+                      child: Icon(
+                        FontAwesomeIcons.userFriends,
+                        color: selectedIndex == 2 ? highLightColor : Colors.white,
+                      ),
+                      onTap: () {
+                        setState(() {
+                          if(selectedIndex != 2) {
+                            selectedIndex = 2;
+                          } else {
+                            selectedIndex = -1;
+                          }
+                        });
+                      },
+                    ),
+                  ),
+                ),
               ),
-              onTap: () {
-                setState(() {
-                  selectedIndex = 2;
-                });
-              },
-            ),
+            ]
           ),
-        ]
-      ),
+        ),
+      ],
     );
   }
 }
